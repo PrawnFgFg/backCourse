dbf36aa391a49725808e7d2bcf8ada744a78e71a
fodui <nikolov7856@mail.ru>
2025-09-01 18:31:22 +0700

    after solution pattern per

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,5 +1,6 @@
 from sqlalchemy import select, Result, insert, literal_column
 from sqlalchemy.ext.asyncio import AsyncSession
+from pydantic import BaseModel
 
 class BaseRepository:
     model = None
@@ -17,24 +18,17 @@ class BaseRepository:
     async def get_one_or_none(self, **filter_by):
         query = select(self.model).filter_by(**filter_by)
         result: Result  = await self.session.execute(query)
-        return result.scalars().one_or_none
+        return result.scalars().one_or_none()
 
     
-    async def add(self, schemas):
-        # add_hotel_stmt = insert(self.model).values(**schemas.model_dump())
-        # result: Result = await self.session.execute(add_hotel_stmt)
-        # res = result.scalars().one_or_none
-        # print(res)
-        # return res
+    async def add(self, schemas: BaseModel):
+        add_hotel_stmt = insert(self.model).values(**schemas.model_dump()).returning(self.model)
+        result: Result = await self.session.execute(add_hotel_stmt)
+        res = result.scalars().one()
+        return res
     
-        new_instance = self.model(**schemas.model_dump())
-        self.session.add(new_instance)
-        return new_instance
         
-
-
-
-        
+   
         
         
         
